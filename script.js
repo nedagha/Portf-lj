8d37c0365a1c86ee26106e08d2f5eb2dd7ac7393
nedagha <97517051+nedagha@users.noreply.github.com>
2022-02-06 09:04:19 +0100

    added some more text in <p> tag in html and added a js code for the scroll up button

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 function myFunction() {
     alert("Message has been sent");
   }
+// scroll-up button show/hide script
+        if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
